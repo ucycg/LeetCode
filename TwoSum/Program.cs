1d4d801f7e78782578a0586fed34001bfa355379
ucycg <ucycg@studen.kit.edu>
2026-05-03 16:21:04 +0200

    Whoop my first LeetCode Solution: TwoSum

---
+++ TwoSum/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.IO.Pipelines;
+
+int[] nums = [2,7,11,15];
+    int target = 9;
+    
+    static int[] TwoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for(int j = i + 1; j < nums.Length; j++ )
+            {
+                if( nums[i] + nums[j] == target)
+                {
+                    int[] result = {i,j};
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+
+    static int[] TwoSumDict(int[] nums, int target) 
+    {
+        Dictionary<int,int> SeenNums  = new Dictionary<int,int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            // cur + x = target
+            int x = target - nums[i];
+            if(SeenNums.TryGetValue(x,out int value))
+                return [value, i];
+            SeenNums.TryAdd(nums[i],i);
+        }
+        return null;
+    }
+
+    Console.WriteLine($"Indices brute force: {string.Join(", ",TwoSum(nums,target))}");
+
+    Console.WriteLine($"Indices with Dictionary: {string.Join(", ",TwoSumDict(nums,target))}");
+    
+
