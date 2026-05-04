2f156b53a392e1c7d513472e5a7582fd34c1badb
Justin Cott <jcott@initse.com>
2026-05-04 15:05:09 +0200

    First Vertical Solution of Longest Common Prefix

--- Arrays/LongestCommonPrefix/Program.cs
+++ Arrays/LongestCommonPrefix/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿string[] strs = ["flower","flow","flight"];
+
+static string LongestCommonPrefix(string[] strs)
+{
+    if (strs == null || strs.Length == 0) return string.Empty;
+    if (strs.Length == 1) return strs[0];
+
+    char[] prefix = strs[0].ToCharArray();
+    char[][] words = new char[200][];
+
+    for(int i = 0; i < strs.Length; i++)
+    {
+        words[i] = strs[i].ToCharArray();
+    }
+
+    for(int i = 0; i < prefix.Length; i++)
+    {
+        for(int j = 0; j < strs.Length; j++)
+        {
+            if (i == words[j].Length || words[j][i] != prefix[i])
+            {
+                return strs[j][..i];
+            }
+        }
+    }
+    return strs[0];
+}
+
+Console.WriteLine(LongestCommonPrefix(strs));
