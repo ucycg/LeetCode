ba5aa80c9407428a729d29e432be64610d205b1a
Justin Cott <jcott@initse.com>
2026-05-07 14:00:12 +0200

    First Idea / Concept for a Brute Force Approach to this problem

--- Arrays/ResultantArrayAfterRemovingAnagrams/Program.cs
+++ Arrays/ResultantArrayAfterRemovingAnagrams/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿static List<string> RemoveAnagrams(string[] words)
+{
+    List<string> CleanWords = [.. words];
+    if(words.Length == 0 || words.Length == 1)
+        return CleanWords;
+    else
+    {
+        while(isAnagram())
+        {
+         i   
+        }
+    }
+    return CleanWords;
+}
+
+static bool isAnagram(string firstWord, string secondWord)
+{
+    if(firstWord.Length == secondWord.Length)
+    {
+        char[] firstWordLetters = new char[26];
+        char[] secondWordLetters = new char[26];
+        return true;
+    }
+    else 
+        return false;
+}
