9b6037ee9372c577d5c58f735d23edc552e05631
Justin Cott <jcott@initse.com>
2026-05-08 14:41:27 +0200

    Working Solution

--- Arrays/ConcatenationOfArrays/Program.cs
+++ Arrays/ConcatenationOfArrays/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿int[] nums = [1,2,1];
+
+static int[] GetConcatenation(int[] nums)
+{
+    int[] concatNums = new int[2*nums.Length];
+    for(int i = 0; i < nums.Length; i++)
+    {
+        concatNums[i] = nums[i];
+        concatNums[nums.Length + i] = nums[i];
+    }
+    return concatNums;
+}
+
+Console.WriteLine(string.Join(",", GetConcatenation(nums)));
